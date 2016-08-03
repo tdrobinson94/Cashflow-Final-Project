c72ff3d55d753d7028da49834ae7fa5a0042f8c6
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-03 18:02:24 -0400

    fixed hamburger menu bug

--- src/js/controllers/layout.controller.js
+++ src/js/controllers/layout.controller.js
@@ -3,12 +3,42 @@ import $ from 'jquery';
 function LayoutController(){
   let vm = this;
 
-$('.hamburger').on('click', function(event){
-  $(this).toggleClass('is-active')
-  $('.links').slideToggle('slow')
-})
+  vm.setNavigation = setNavigation;
+  vm.init = init;
 
+  function init() {
+    setNavigation();
+    hamburgerHandler();
+    linksHandler();
+  };
 
+  init();
+
+  function setNavigation () {
+    if ($(window).width() < 450) {
+      $('.links').hide();
+    } else {
+      $('.links').show();
+    }
+  };
+
+  function hamburgerHandler() {
+    $(".hamburger").on("click", () => {
+      $('.hamburger').toggleClass('is-active')
+      $(".links").slideToggle();
+    });
+  };
+
+  function linksHandler() {
+    $(".navbar a").on("click", () => {
+      if ($(window).width() < 450) {
+        $(".hamburger").removeClass("is-active")
+        $('.links').slideToggle();
+      }
+    })
+  };
+
+  $(window).resize(setNavigation)
 
 
 }
