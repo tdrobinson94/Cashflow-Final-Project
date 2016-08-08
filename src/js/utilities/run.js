e0fd8c4b3f225bf53792dcab39b590e1433586ac
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-08 18:48:48 -0400

    allowed the home page to be used whenn you arent logged in

--- src/js/utilities/run.js
+++ src/js/utilities/run.js
@@ -2,9 +2,9 @@ function run($rootScope, LoginService, $state){
 
 
   $rootScope.$on('$stateChangeStart', function(event, toState){
-    if (!LoginService.loggedIn() && toState.name !== 'root.log-in' && toState.name !== 'root.sign-up'){
+    if (!LoginService.loggedIn() && toState.name !== 'root.log-in' && toState.name !== 'root.sign-up' && toState.name !== 'root.home'){
       event.preventDefault();
-      $state.go('root.log-in');
+      $state.go('root.home');
     }
   })
 
