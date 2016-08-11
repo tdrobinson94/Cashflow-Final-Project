bb051e005b7ace265408aeb113d868c6ef786648
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-10 20:13:49 -0400

    logging in now take you to the accounts page

--- src/js/controllers/profile.controller.js
+++ src/js/controllers/profile.controller.js
@@ -3,19 +3,29 @@ import $ from 'jquery';
 function ProfileController(UserService, $cookies) {
 
   let vm = this;
+  vm.enterAccountInfo = enterAccountInfo;
+  vm.accountInput = accountInput;
+  vm.account = [];
+  vm.showAccountInput = {
+		accountInput: false
+	};
 
-  init();
+  function enterAccountInfo(account){
+		vm.showAccountInput.accountInput = true;
+	}
 
-  function init(){
-    let user = $cookies.get('access_token')
-    getUser(id);
-  }
-
-  function getUser(id){
-    UserService.getUser(id).then(function(res){
-      console.log(res);
-    })
-  }
+  // init();
+  //
+  // function init(){
+  //   let user = $cookies.get('access_token')
+  //   getUser(id);
+  // }
+  //
+  // function getUser(id){
+  //   UserService.getUser(id).then(function(res){
+  //     console.log(res);
+  //   })
+  // }
 
 }
 
