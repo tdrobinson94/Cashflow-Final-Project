858147df9ce1c68ba81530091960a0b2b69b3bc7
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-11 12:37:21 -0400

    signup now throws alert error when someone uses and email or username that has already been used by someone else

--- src/js/controllers/signup.controller.js
+++ src/js/controllers/signup.controller.js
@@ -7,7 +7,11 @@ function SignUpController($http, SERVER, $state, UserService) {
   function signup(user){
     UserService.signup(user).then(function(res){
       console.log(res);
-      $state.go('root.log-in');
+      if (res.data[0].message ){
+        alert('Email or Username has been taken by someone else')
+      }else {
+        $state.go('root.log-in');
+      }
     })
   }
 
