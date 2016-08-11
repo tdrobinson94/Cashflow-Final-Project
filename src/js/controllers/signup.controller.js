31e781dbff037474da4bfcd2b8ec3c5036ded05d
tdrobinson94 <tdrobinson94@gmail.com>
2016-08-11 16:26:26 -0400

    created a better validation method using status changes

--- src/js/controllers/signup.controller.js
+++ src/js/controllers/signup.controller.js
@@ -8,10 +8,10 @@ function SignUpController($http, SERVER, $state, UserService) {
     UserService.signup(user).then(function(res){
       console.log(res);
       // Refactor this code using try and catch
-      if (res.data[0].message ){
-        alert('Email or Username has been taken by someone else')
-      }else {
+      if (res.status === 201){
         $state.go('root.log-in');
+      }else {
+        alert('Email or Username has been taken by someone else')
       }
     })
   }
